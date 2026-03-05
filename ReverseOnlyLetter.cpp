b1c8d9d48bc6d482ce9198bfe30affc8a70415f9
Hydra-Ramesh <dasramesh8343003905@gmail.com>
2026-03-05 23:35:50 +0530

    Leetcode 917

---
+++ ReverseOnlyLetter.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string reverseOnlyLetters(string s) {
+        int i = 0, j = s.size() - 1;
+
+        while(i < j) {
+            if(isalpha(s[i]) && isalpha(s[j])) {
+                swap(s[i], s[j]);
+                i++;
+                j--;
+            }
+            else if(!isalpha(s[i])) {
+                i++;
+            }
+            else {
+                j--;
+            }
+        }
+        return s;
+    }
+};
+
+int main() {
+    Solution sol;
+    string s;
+
+    cout << "Enter string: ";
+    getline(cin, s);
+
+    cout << "Output: " << sol.reverseOnlyLetters(s);
+
+    return 0;
+}
